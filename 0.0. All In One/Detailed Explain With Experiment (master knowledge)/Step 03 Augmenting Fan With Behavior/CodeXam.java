d930b6b2777757810c65a61aa151dc3206fad2b8
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-09-13 19:12:25 +0530

    0.0 All in One -> Step 03 Augmenting Fan With Behavior
    Step 03 Augmenting Fan With Behavior

---
+++ 0.0. All In One/Detailed Explain With Experiment (master knowledge)/Step 03 Augmenting Fan With Behavior/CodeXam.java
@@ -0,0 +1,59 @@
+  class Fan {
+    //state
+    private String make;
+    private double radius;
+    private String color;
+    private boolean isOn;
+    private byte speed;	//levels: 0 to 5
+
+    //constructors
+    public Fan(String make, double radius, String color) {
+        this.make = make;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    //methods
+    public String toString() {
+        return String.format("Make : %s, Radius : %f, Color : %s, Is On : %b, Speed : %d",
+                make,
+                radius,
+                color,
+                isOn,
+                speed);
+    }
+
+    //isOn
+		/*public void isOn(boolean isOn) {
+			this.isOn = isOn;
+		}*/
+
+    public void switchOn() {
+        isOn = true;
+        setSpeed((byte)1);
+    }
+
+    public void switchOff() {
+        isOn = false;
+        setSpeed((byte)0);
+    }
+
+    public void setSpeed(byte speed) {
+        this.speed = speed;
+    }
+}
+public class CodeXam {
+    public static void main(String[] args) {
+        Fan fan = new Fan("Fan-Tastic", 0.456, "GREEN");
+        System.out.println(fan);
+        fan.switchOn();
+        System.out.println(fan);
+        fan.setSpeed((byte)5);
+        System.out.println(fan);
+        fan.switchOff();
+        System.out.println(fan);
+    }
+}
+
+
+
